7f2c2094d248eb3a0a03ae832eb5e4d544088673
Mateus7799 <136096790+Mateus7799@users.noreply.github.com>
2023-10-20 12:17:38 -0300

    PlayerHeapsort.java

--- AEDs-02/Questão 9/PlayerHeapsort.java
+++ AEDs-02/Questão 9/PlayerHeapsort.java
@@ -1,138 +1,101 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 class Player {
-    int id;
-    String player;
-    int height;
-    int weight;
+    String id;
+    String name;
+    String height;
+    String weight;
+    String college;
     String born;
-    String collage;
     String birthCity;
     String birthState;
 
-    public Player(int id, String player, int height, int weight, String born, String collage, String birthCity, String birthState) {
+    public Player(String id, String name, String height, String weight, String college, String born, String birthCity, String birthState) {
         this.id = id;
-        this.player = player;
+        this.name = name;
         this.height = height;
         this.weight = weight;
+        this.college = college;
         this.born = born;
-        this.collage = collage;
         this.birthCity = birthCity;
         this.birthState = birthState;
     }
 
     @Override
     public String toString() {
-        return "[" + id + " ## " + player + " ## " + height + " ## " + weight + " ## " + born + " ## " + collage + " ## " + birthCity + " ## " + birthState + "]";
+        return String.format("[Id %s Player %s height %s weight %s born %s collage %s birth city %s birth state %s]",
+                id, name, height, weight, born, college, birthCity, birthState);
     }
 }
 
-public class PlayerHeapsort {
-    static int comparisons = 0;
-    static int movements = 0;
+public class PlayerHeapSort {
+    private static int comparisons = 0;
+    private static int movements = 0;
 
-    public static void heapify(Player[] arr, int n, int i) {
+    private static void heapify(ArrayList<Player> arr, int n, int i) {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
 
-        if (l < n && arr[l].height > arr[largest].height) {
+        if (l < n && Integer.parseInt(arr.get(l).height) > Integer.parseInt(arr.get(largest).height)) {
             largest = l;
         }
-
-        if (r < n && arr[r].height > arr[largest].height) {
+        if (r < n && Integer.parseInt(arr.get(r).height) > Integer.parseInt(arr.get(largest).height)) {
             largest = r;
         }
+        comparisons += 2;
 
         if (largest != i) {
-            Player temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
+            Player temp = arr.get(i);
+            arr.set(i, arr.get(largest));
+            arr.set(largest, temp);
             movements += 3;
             heapify(arr, n, largest);
         }
     }
 
-    public static void heapSort(Player[] arr) {
-        int n = arr.length;
+    private static void heapSort(ArrayList<Player> arr) {
+        int n = arr.size();
 
         for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
 
         for (int i = n - 1; i > 0; i--) {
-            Player temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+            Player temp = arr.get(0);
+            arr.set(0, arr.get(i));
+            arr.set(i, temp);
             movements += 3;
+
             heapify(arr, i, 0);
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            File file = new File("/tmp/players.csv");
-            Scanner sc = new Scanner(file);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/tmp/players.csv"));
+        PrintWriter writer = new PrintWriter(new FileWriter("808360_heapsort.txt"));
 
-            ArrayList<Player> players = new ArrayList<>();
+        ArrayList<Player> players = new ArrayList<>();
+        String line = reader.readLine(); // ignore first line
 
-            if (sc.hasNextLine()) {
-                sc.nextLine();
-            }
-
-            while (sc.hasNextLine()) {
-                String[] data = sc.nextLine().split(",");
-                int id = Integer.parseInt(data[0].trim());
-                String player = data[1].trim();
-                int height = Integer.parseInt(data[2].trim());
-                int weight = Integer.parseInt(data[3].trim());
-                String collage = data[4].trim();
-                String born = data[5].trim();
-                String birthCity = data[6].trim();
-                String birthState = data[7].trim();
-
-                players.add(new Player(id, player, height, weight, born, collage, birthCity, birthState));
-            }
-            sc.close();
-
-            Scanner inputScanner = new Scanner(System.in);
-
-            ArrayList<Integer> ids = new ArrayList<>();
-            while (true) {
-                String input = inputScanner.nextLine();
-                if (input.equals("FIM")) {
-                    break;
-                }
-                ids.add(Integer.parseInt(input));
-            }
-            inputScanner.close();
-
-            Player[] selectedPlayers = new Player[ids.size()];
-            int idx = 0;
-            for (Player p : players) {
-                if (ids.contains(p.id)) {
-                    selectedPlayers[idx] = p;
-                    idx++;
-                }
-            }
-
-            heapSort(selectedPlayers);
-
-            for (Player p : selectedPlayers) {
-                System.out.println(p.toString());
-            }
-
-            FileWriter logFile = new FileWriter("808360_heapsort.txt");
-            logFile.write("808360\t" + comparisons + "\t" + movements + "\t" + "TimeTaken" + "\n");
-            logFile.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing the log file.");
-            e.printStackTrace();
+        while ((line = reader.readLine()) != null && !line.equals("FIM")) {
+            String[] data = line.split(",");
+            players.add(new Player(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]));
         }
+        reader.close();
+
+        heapSort(players);
+
+        for (Player player : players) {
+            System.out.println(player.toString());
+        }
+
+        writer.print("808360\t" + comparisons + "\t" + movements + "\t" + System.currentTimeMillis());
+        writer.close();
     }
 }
